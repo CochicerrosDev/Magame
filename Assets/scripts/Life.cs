418c713322709fd05ba5e57e5fea12e285c195e5
lumilo8 <lumilo8@gmail.com>
2015-02-10 18:28:27 +0100

    Life script, enemies died

---
+++ Assets/scripts/Life.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TYPE {
+	PLAYER,
+	MAGIC,
+	ENEMY
+}
+public class Life : MonoBehaviour {
+
+	public TYPE type;
+	public int life = 50;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Life otherLife = other.gameObject.GetComponent ("Life") as Life;
+			
+		// Si el objeto es de tipo magia y con el que choca es de tipo enemigo, hace daño 100
+		if (type == TYPE.MAGIC && otherLife.type == TYPE.ENEMY) {
+			otherLife.life -= 100;
+		}
+		// Si la vida del otro objeto esta por debajo de 0, se destruye
+		if (otherLife.life <= 0)
+			Destroy (other.gameObject);
+		
+		// Si es de tipo magia, se destruye al chocar con algo
+		if(type == TYPE.MAGIC)
+			Destroy (gameObject);
+	
+	}
+}
